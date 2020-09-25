2e527a774e2a0075521d8101a94a83501498dcdf
alphakevin <get.so.smart@gmail.com>
2020-09-25 15:46:38 +0800

    Add raw collection test suit, remove max & min value for statistics

--- server/methods.js
+++ server/methods.js
@@ -18,39 +18,60 @@ function generateData(size = 1) {
   return data;
 }
 
-function benchmark(name, repeat, fn) {
-  console.log(`benchmark "${name}": repeat=${repeat}`);
-  const durations = Array(repeat);
-  let total = 0;
-  for (let i = 0; i < repeat; i++) {
-    process.stdout.write(`loop ${i}...`);
-    const startTime = performance.now();
-    fn();
-    const endTime = performance.now();
-    durations[i] = endTime - startTime;
-    total += durations[i];
-    process.stdout.write(`finished in ${durations[i]}ms\n`);
-  }
-  const average = total / repeat;
-  console.log(`average time ${total / repeat}`);
-  return { durations, average };
+function statistic(_list) {
+  const count = _list.length;
+  const list = [..._list].sort();
+  list.sort();
+  const max = list.pop();
+  const min = list.shift();
+  const total = list.reduce((prev, current) => prev + current);
+  const average = total / (count - 2);
+  return {
+    count,
+    max,
+    min,
+    average,
+  };
 }
 
+async function benchmark(name, repeat, fn) {
+  console.log(`benchmark "${name}": repeat=${repeat}`);
+  const durations = Array(repeat);
+  for (let i = 0; i < repeat; i++) {
+    const startTime = performance.now();
+    await fn();
+    const endTime = performance.now();
+    durations[i] = endTime - startTime;
+    console.log(`loop ${i} finished in ${durations[i]}ms`);
+  }
+  const result = statistic(durations);
+  console.log(`${name} all done`);
+  console.dir(result);
+  return result;
+}
 
 Meteor.methods({
-  test1(repeat = 10, limit = 100) {
-    return benchmark('test1', repeat, () => {
+  async test1(repeat = 10, limit = 100) {
+    await benchmark('test1', repeat, async () => {
       const activities = Activities.find({}, { limit }).fetch();
       activities.forEach(activity => {
         fn1(activity);
       });
     });
   },
-  test2(repeat = 10, limit = 100) {
-    return benchmark('test2', repeat, () => {
+  async test2(repeat = 10, limit = 100) {
+    await benchmark('test2', repeat, async () => {
       const activities = ActivitiesWithHelper.find({}, { limit }).fetch();
       activities.forEach(activity => {
         activity.method1();
+      });
+    });
+  },
+  async test3(repeat = 10, limit = 100) {
+    await benchmark('test3', repeat, async () => {
+      const activities = await Activities.rawCollection().find({}, { limit }).toArray();
+      activities.forEach(activity => {
+        fn1(activity);
       });
     });
   },
